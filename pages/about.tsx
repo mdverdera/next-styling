57bf97bd7b12b31ec90746aa2f0a8de8ee079551
MadMan24 <mdverdera@yahoo.com>
2022-10-09 12:58:07 +0800

    SASS support for Next JS

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,14 @@
+import styles from "@styles/About.module.css";
+import stylesscss from "@styles/About.module.scss";
+
 const About = () => {
   return (
     <>
       <h2>About</h2>
       <button className="btn btn-primary">Primary</button>
       <button className="btn btn-success">Success</button>
+      <div className={styles.highlight}>Local CSS for About Page</div>
+      <div className={stylesscss.highlightscss}>Local SCSS for About Page</div>
     </>
   );
 };
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,14 @@
+import styles from "@styles/Contact.module.css";
+import stylesscss from "@styles/Contact.module.scss";
+
 const Contact = () => {
-  return <h2>Contact</h2>;
+  return (
+    <>
+      <h2>Contact</h2>{" "}
+      <div className={styles.highlight}>Local CSS for Contact Page</div>
+      <div className={stylesscss.highlightscss}>Local SCSS for Contac Page</div>
+    </>
+  );
 };
 
 export default Contact;
