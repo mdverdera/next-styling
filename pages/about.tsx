c833fbd6f9fa0d75afe641a157c33963b9d67f00
MadMan24 <mdverdera@yahoo.com>
2022-10-09 12:15:59 +0800

    Initial commit. Applying global styles.

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <>
+      <h2>About</h2>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-success">Success</button>
+    </>
+  );
+};
+export default About;
