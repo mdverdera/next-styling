a443ccd75cd2abab0cde9c571c6d0db9595e0d94
MadMan24 <mdverdera@yahoo.com>
2022-10-09 13:10:26 +0800

    Styled components and inline css.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primary: "#355C7D",
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
